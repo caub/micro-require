bb525e5f6f307417d68aeedb0b5508abb7518b86
Awal Garg <awalgarg@gmail.com>
2016-03-13 23:10:39 +0530

    speed up things

--- src/index.js
+++ src/index.js
@@ -12,12 +12,13 @@ console.time('react require');
 require('react');
 console.timeEnd('react require'); // averages ~1.7s on chrome, ~2.5s on FF
 
-console.time('dupe react require');
+/*console.time('dupe react require');
 require('react');
 console.timeEnd('dupe react require'); // instant, as it should be
 
 console.time('vdom');
 require('virtual-dom');
 console.timeEnd('vdom'); // averages ~0.6s on chrome, ~0.9s on FF
+*/
 
 console.log(require('react').renderToString(require('./devil1/foo.jsx')));
